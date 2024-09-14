e1019dc88835c5709b47d21d852a177d988e5557
imranulHoque <imrancoder.javascript@gmail.com>
2024-09-14 10:38:11 +0600

    first comit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Floating } from "./components/floating";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-gray-300"> 
+      <Floating />
+    </div>
+  )
+}
+
+export default App;
